fba1640b342100e4726dde5b48cfa4155ed4253c
SaiTheinThanTun <theinthantun.sai@gmail.com>
2015-07-22 18:27:10 +0700

    getting to ode

--- CUA_HIV_Rx.R
+++ CUA_HIV_Rx.R
@@ -7,10 +7,12 @@ parameters <- c(
   R0 <- 5, #Sexual contact (2-5) #wikipedia. Treatment could increase by 2- http://cid.oxfordjournals.org/content/44/8/1115.full% , and this does not consider how effect of transmission could be modified by increased longevity (mind you, in so far as transmission rate is increases that MIGHT reduce longevity if life expectancy on therapy is still lower than if not infection)
   #Rx_cov <- .3, #ART treatment coverage  change this, it's currently for those who 'shoudl be on treatment', we want all HIV positive.
   Rx_cov <- 6.2/23.5, #treatment coverage #avert.org
-  time <- 50, #years
+  
   <- life_expectancy #average SURVIVAL TIME for HIV positive without treametn is 9-11 years (wikipedia)
   
 )
+
+time <- 0:50 , #years
 
 cost_par <- c(Rx_costyr= 140, #in USD per person per year
               AIDS_cost =    745.27, #cost of treatment of AIDS per patient year in 2015 US$. From page 177 of Confronting AIDS: Public Priorities in a Global Epidemic
@@ -36,8 +38,30 @@ HIV_Rx <- function(t, state, parms)
   with(as.list(c(state, parms)),
        {
          
+         P <- S+Ia+Ib
+         lam <- R0
          
+         #rate of change
+         dS <- mui*P-muoS*S-lam*S
+         dIa <- -muoIa*Ia+lam*S-trans*Ia
+         dIb <- -muoIb*Ib+trans*Ia
+         dD <- muoS*S+muoIa*Ia+muoIb*Ib
          
+         # return the rate of change
+         list(c(dS, dIa, dIb, dY))
          
-         
-         
+       }
+  ) 
+  
+}
+
+############################
+#Basecase output
+############################
+out <- ode(y = state, times = time, func = HIV_Rx, parms = parameters)
+
+
+
+
+
+
