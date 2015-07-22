56c1aa0bd5a412f522ca3fdda214177e30e67cfa
SaiTheinThanTun <theinthantun.sai@gmail.com>
2015-07-22 22:41:05 +0700

    death category removed

--- CUA_HIV_Rx.R
+++ CUA_HIV_Rx.R
@@ -1,28 +1,33 @@
 #Cost-utility analysis of treating all HIV patients
 
 library(deSolve)
+
+
 parameters <- c(
-  mui = 1/50 , #birth ?dP rate of population change?
-  muoS = 1/50 , #death rate for normal people
-  muoIa = 1/20 , #death rate for HIV stage I/II
-  muoIb = 1/5 , #death rate for HIV stage III/IV
-  trans = 1/10, #transition from Ia to Ib
-  R0 <- 5, #Sexual contact (2-5) #wikipedia. Treatment could increase by 2- http://cid.oxfordjournals.org/content/44/8/1115.full% , and this does not consider how effect of transmission could be modified by increased longevity (mind you, in so far as transmission rate is increases that MIGHT reduce longevity if life expectancy on therapy is still lower than if not infection)
-  #Rx_cov <- .3, #ART treatment coverage  change this, it's currently for those who 'shoudl be on treatment', we want all HIV positive.
-  Rx_cov <- 6.2/23.5, #treatment coverage #avert.org
+  mui = 1/50, 				#birth ?dP rate of population change?
+  muoS = 1/50, 				#death rate for normal people 12.6 per 1000 person years (ref ???trading economics???)
+  muoIa = 1/20, 				#death rate for HIV stage I/II
+  muoIb = 1/5, 				#death rate for HIV stage III/IV
+  time_without_drugs <- 5,
+  resistance <- 1 + time_without_drugs,
+  trans = 1/10*resistance, 		#transition from Ia to Ib
+  R0 <- 5, #Sexual contact (2-5) 	#wikipedia. Treatment could increase by 2- http://cid.oxfordjournals.org/content/44/8/1115.full% , and this does not consider how effect of transmission could be modified by increased longevity (mind you, in so far as transmission rate is increases that MIGHT reduce longevity if life expectancy on therapy is still lower than if not infection)
+  #Rx_cov <- .3, 			#ART treatment coverage  change this, it's currently for those who 'shoudl be on treatment', we want all HIV positive.
+  Rx_cov <- 6.2/23.5, 			#treatment coverage #avert.org
   
-  LE		<- 10 #life_expectancy #average SURVIVAL TIME for HIV positive without treametn is 9-11 years (wikipedia)
+  LE <- 10 				#life_expectancy #average SURVIVAL TIME for HIV positive without treametn is 9-11 years (wikipedia)
+  
   
 )
-
-time <- 0:50  #years
-
-cost_par <- c(Rx_costyr= 140, #in USD per person per year
-              AIDS_cost =    745.27, #cost of treatment of AIDS per patient year in 2015 US$. From page 177 of Confronting AIDS: Public Priorities in a Global Epidemic
-              costIa = 745.27*.3, #Rx cost for HIV stage I/II
-              costIb = 745.27*.7 #Rx cost for HIV stage III/IV
+time <- 0:50  				#years
+cost_par <- c(Rx_costyr= 140, 	#in USD per person per year
+              AIDS_cost =    745.27, 		#cost of treatment of AIDS per patient year in 2015 US$. From page 177 of Confronting AIDS: Public Priorities in a Global Epidemic
+              costIa = 140, 		#Rx cost for HIV stage I/II
+              costIb = 745.27 			#Rx cost for HIV stage III/IV
 )
 
+
+#Initial values
 
 initP <-1*10^9 # http://populationpyramid.net/sub-saharan-africa/2015/
 dP <- 1.6*10^9/50 #rate of pop change over 50 years
@@ -30,11 +35,14 @@ dP <- 1.6*10^9/50 #rate of pop change over 50 years
 initI <- 23.5*10^6 #avert.org
 initIa <- initI*.3 #HIV stage I/II
 initIb <- initI*.7 #HIV stage III/IV
-initS <- initP-initI
+init_not_S <- 0.15*initP	#From ???http://populationpyramid.net/sub-saharan-africa/2015/??? assuming that those under 10 not infected (not true)
 
-state <- c(S = initS, Ia=initIa, Ib = initIb, D = 0)
+initS <- initP-initI-init_not_S
 
 
+state <- c(S = initS, Ia=initIa, Ib = initIb)
+
+#######################################################################
 
 HIV_Rx <- function(t, state, parms) 
 {
@@ -42,16 +50,16 @@ HIV_Rx <- function(t, state, parms)
        {
          
          P <- S+Ia+Ib
-         lam <- R0 *((Ia+Ib)/P)
+         lam <- R0 *((Ia+Ib)/P) #Force of infection
          
          #rate of change
          dS <- mui*P-muoS*S-lam*S
          dIa <- -muoIa*Ia+lam*S-trans*Ia
          dIb <- -muoIb*Ib+trans*Ia
-         dD <- muoS*S+muoIa*Ia+muoIb*Ib
+         
          
          # return the rate of change
-         list(c(dS, dIa, dIb, dD))
+         list(c(dS, dIa, dIb))
          
        }
   ) 
