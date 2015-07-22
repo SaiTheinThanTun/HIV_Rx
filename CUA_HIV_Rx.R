cc1d67d6cfe8a2f21d3301007460348a2edb11b8
SaiTheinThanTun <theinthantun.sai@gmail.com>
2015-07-22 17:33:24 +0700

    2nd commit

--- CUA_HIV_Rx.R
+++ CUA_HIV_Rx.R
@@ -1,10 +1,10 @@
 #Cost-utility analysis of treating all HIV patients
 parameters <- c(
-  mui =  #birth ?dP rate of population change?
-    muoS =  #death rate for normal people
-    muoIa = #death rate for HIV stage I/II
-    muoIb = #death rate for HIV stage III/IV
-    R0 <- 5, #Sexual contact (2-5) #wikipedia. Treatment could increase by 2- http://cid.oxfordjournals.org/content/44/8/1115.full% , and this does not consider how effect of transmission could be modified by increased longevity (mind you, in so far as transmission rate is increases that MIGHT reduce longevity if life expectancy on therapy is still lower than if not infection)
+  mui = 1/50 , #birth ?dP rate of population change?
+  muoS = 1/50 , #death rate for normal people
+  muoIa = 1/20 , #death rate for HIV stage I/II
+  muoIb = 1/5 , #death rate for HIV stage III/IV
+  R0 <- 5, #Sexual contact (2-5) #wikipedia. Treatment could increase by 2- http://cid.oxfordjournals.org/content/44/8/1115.full% , and this does not consider how effect of transmission could be modified by increased longevity (mind you, in so far as transmission rate is increases that MIGHT reduce longevity if life expectancy on therapy is still lower than if not infection)
   #Rx_cov <- .3, #ART treatment coverage  change this, it's currently for those who 'shoudl be on treatment', we want all HIV positive.
   Rx_cov <- 6.2/23.5, #treatment coverage #avert.org
   time <- 50, #years
@@ -14,7 +14,8 @@ parameters <- c(
 
 cost_par <- c(Rx_costyr= 140, #in USD per person per year
               AIDS_cost =    745.27, #cost of treatment of AIDS per patient year in 2015 US$. From page 177 of Confronting AIDS: Public Priorities in a Global Epidemic
-              
+              costIa = 745.27*.3, #Rx cost for HIV stage I/II
+              costIb = 745.27*.7 #Rx cost for HIV stage III/IV
 )
 
 
@@ -26,8 +27,17 @@ initIa <- initI*.3 #HIV stage I/II
 initIb <- initI*.7 #HIV stage III/IV
 initS <- initP-initI
 
+state <- c(S = initS, Ia=initIa, Ib = initIb, D = 0)
 
 
 
-
-
+HIV_Rx <- function(t, state, parms) 
+{
+  with(as.list(c(state, parms)),
+       {
+         
+         
+         
+         
+         
+         
